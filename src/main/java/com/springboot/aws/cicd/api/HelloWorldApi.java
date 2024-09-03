c468204589e1638a8955a9726d32c447654651fb
MuraliMK <muraleemk@gmail.com>
2024-09-03 19:40:29 +0530

    Fourth commit

--- src/main/java/com/springboot/aws/cicd/api/HelloWorldApi.java
+++ src/main/java/com/springboot/aws/cicd/api/HelloWorldApi.java
@@ -1,6 +1,8 @@
 package com.springboot.aws.cicd.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,11 @@ public class HelloWorldApi {
     @GetMapping
     public String helloWord(){
         return "Om Namo Narayana";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String getFoos(@RequestParam String id) {
+        return "Hello Mr." + id;
     }
 }
